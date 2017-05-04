83184e1958b6008d0b5f042cf3c2e85f95644135
Kevin <b03902086@ntu.edu.tw>
2017-05-05 03:14:04 +0800

    main

--- src/embed.py
+++ src/embed.py
@@ -51,66 +51,67 @@ def reviews2vec_review(reviews):
     return vec_review
 
 
-start = time()
-# TODO: noises (review, +1) and (review, -1)
-review_pol = {}
-with open('../data/polarity_review_seg.txt') as seg_file:
-    for line in seg_file:
-        pol, review = line.strip().split(' ')
-        pol = int(pol)
-        review_pol[review] = pol
+if __name__ == '__main__':
+    start = time()
+    # TODO: noises (review, +1) and (review, -1)
+    review_pol = {}
+    with open('../data/polarity_review_seg.txt') as seg_file:
+        for line in seg_file:
+            pol, review = line.strip().split(' ')
+            pol = int(pol)
+            review_pol[review] = pol
 
-vec_review_25w = reviews2vec_review(review_pol)
-delta = time() - start
-info('finish reading polarity_review', delta)
+    vec_review_25w = reviews2vec_review(review_pol)
+    delta = time() - start
+    info('finish reading polarity_review', delta)
 
-start = time()
-review_info = {}
-with open('../data/aspect_review_seg.txt') as seg_file:
-    lines = seg_file.readlines()
-    num = len(lines) // 4
-    for i in range(num):
-        id_, review, pos, neg = map(strip, lines[4*i : 4*(i+1)])
-        id_ = int(id_)
-        # pos = pos.split()
-        # neg = neg.split()
-        review_info[review] = (pos, neg)
+    start = time()
+    review_info = {}
+    with open('../data/aspect_review_seg.txt') as seg_file:
+        lines = seg_file.readlines()
+        num = len(lines) // 4
+        for i in range(num):
+            id_, review, pos, neg = map(strip, lines[4*i : 4*(i+1)])
+            id_ = int(id_)
+            # pos = pos.split()
+            # neg = neg.split()
+            review_info[review] = (pos, neg)
 
-vec_review_200 = reviews2vec_review(review_info)
-delta = time() - start
-info('finish reading aspect_review', delta)
+    vec_review_200 = reviews2vec_review(review_info)
+    delta = time() - start
+    info('finish reading aspect_review', delta)
 
-vecs25w = list(vec_review_25w.keys())
-vecs200 = list(vec_review_200.keys())
-dim = len(vecs25w), len(vecs200)
-start = time()
-cosine = cdist(vecs25w, vecs200, metric='cosine')
-delta = time() - start
-info('finish creating cosine matrix', delta)
+    vecs25w = list(vec_review_25w.keys())
+    vecs200 = list(vec_review_200.keys())
+    dim = len(vecs25w), len(vecs200)
+    start = time()
+    cosine = cdist(vecs25w, vecs200, metric='cosine')
+    delta = time() - start
+    info('finish creating cosine matrix', delta)
 
-similar = cosine.argsort(1)[:, 0]
-# sort[i]: most similar to least similar aspect_reviews to i-th polarity_review
-start = time()
-with open('../data/polarity_review_aspect.txt', 'w') as out_file:
-    for i, j in enumerate(similar):
-        vec_i, vec_j = vecs25w[i], vecs200[j]
-        review_i, review_j = vec_review_25w[vec_i], vec_review_200[vec_j]
-        pos, neg = review_info[review_j]
-        
-        # TODO: decide which aspects to take
-        # maybe dont take all, or also take opposite ones
-        if review_pol[review_i] > 0:
-            if pos != '':
-                out_file.write(review_i + '\n')
-                out_file.write(pos + '\n')
-                out_file.write('\n')
-                out_file.write('%.6f\n' % cosine[i, j])
-        else:
-            if neg != '':
-                out_file.write(review_i + '\n')
-                out_file.write('\n')
-                out_file.write(neg + '\n')
-                out_file.write('%.6f\n' % cosine[i, j])
-                
-delta = time() - start
-info('finish output file', delta)
+    similar = cosine.argsort(1)[:, 0]
+    # sort[i]: most similar to least similar aspect_reviews to i-th polarity_review
+    start = time()
+    with open('../data/polarity_review_aspect.txt', 'w') as out_file:
+        for i, j in enumerate(similar):
+            vec_i, vec_j = vecs25w[i], vecs200[j]
+            review_i, review_j = vec_review_25w[vec_i], vec_review_200[vec_j]
+            pos, neg = review_info[review_j]
+            
+            # TODO: decide which aspects to take
+            # maybe dont take all, or also take opposite ones
+            if review_pol[review_i] > 0:
+                if pos != '':
+                    out_file.write(review_i + '\n')
+                    out_file.write(pos + '\n')
+                    out_file.write('\n')
+                    out_file.write('%.6f\n' % cosine[i, j])
+            else:
+                if neg != '':
+                    out_file.write(review_i + '\n')
+                    out_file.write('\n')
+                    out_file.write(neg + '\n')
+                    out_file.write('%.6f\n' % cosine[i, j])
+                    
+    delta = time() - start
+    info('finish output file', delta)
